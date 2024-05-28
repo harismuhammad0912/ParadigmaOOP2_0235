dce06c7c06d53173ede0b68b8a736f68324a08b3
harismuhammad0912 <muhammadharis091204@gmail.com>
2024-05-28 16:17:02 +0700

    3

--- no_pewaris.cpp
+++ no_pewaris.cpp
@@ -7,4 +7,16 @@ public:
 		cout << "Hallo saya Function dari base class";
 	}
 };
+class derivedClass : public baseClass {
+public:
+	void perkenalan() {
+		cout << "Hallo saya Function dari derived Class";
+	}
+};
 
+int main() {
+	derivedClass a;
+	a.perkenalan();
+
+	return 0;
+}
